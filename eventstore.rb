507393c45515a68c12f8bcb686f42114f06386b8
Yaroslav Klymko <t3hnar@users.noreply.github.com>
2016-08-31 08:02:08 +0300

    eventstore 3.9.0

--- eventstore.rb
+++ eventstore.rb
@@ -1,8 +1,8 @@
 class Eventstore < Formula
   desc "functional database with complex event processing"
   homepage "http://geteventstore.com"
-  url "http://download.geteventstore.com/binaries/EventStore-OSS-MacOSX-v3.7.0.tar.gz"
-  sha256 "e5902899d45aca6414fae01fff152ddc2df1495ad29d36ea3bcffbe33025e396"
+  url "http://download.geteventstore.com/binaries/EventStore-OSS-MacOSX-v3.9.0.tar.gz"
+  sha256 "8b89af0e29a0cc92b4f04d7586426f58191896905f47369938d34a69dcc880f2"
 
   bottle :unneeded
 
